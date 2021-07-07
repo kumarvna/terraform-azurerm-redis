c8023a9ddd620cda90edc1b37b211d5631e5932b
Kumaraswamy Vithanala <36370570+kumarvna@users.noreply.github.com>
2021-07-07 18:02:21 +0530

    initial commit

---
+++ examples/complete/main.tf
@@ -0,0 +1,21 @@
+module "redis" {
+  //  source  = "kumarvna/redis/azurerm"
+  //  version = "1.0.0"
+  source = "../../"
+
+  # By default, this module will create a resource group
+  # proivde a name to use an existing resource group and set the argument 
+  # to `create_resource_group = false` if you want to existing resoruce group. 
+  # If you use existing resrouce group location will be the same as existing RG.
+  create_resource_group = false
+  resource_group_name   = "rg-shared-westeurope-01"
+  location              = "westeurope"
+
+
+  # Tags for Azure Resources
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Owner       = "test-user"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,23 @@
+#------------------------------------------------------------
+# Local configuration - Default (required). 
+#------------------------------------------------------------
+
+locals {
+  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
+  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
+}
+
+#---------------------------------------------------------
+# Resource Group Creation or selection - Default is "false"
+#----------------------------------------------------------
+data "azurerm_resource_group" "rgrp" {
+  count = var.create_resource_group == false ? 1 : 0
+  name  = var.resource_group_name
+}
+
+resource "azurerm_resource_group" "rg" {
+  count    = var.create_resource_group ? 1 : 0
+  name     = var.resource_group_name
+  location = var.location
+  tags     = merge({ "Name" = format("%s", var.resource_group_name) }, var.tags, )
+}
